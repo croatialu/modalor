fed0c1483c770d25f48988687d1814f325bef69f
croatialu <wuli.croatia@foxmail.com>
2024-11-06 01:08:38 +0800

    feat(react): @modalor/react

---
+++ packages/react/README.md
@@ -0,0 +1,95 @@
+# @modalor/vue
+
+A modal state manager for Vue3.
+
+## Install
+
+```bash
+pnpm add @modalor/vue
+```
+
+## Usage
+
+1. Render the Modalor in the root of your app.
+
+```vue
+<script lang="ts" setup>
+import { Modalor } from '@modalor/vue'
+</script>
+
+<!-- App.vue -->
+<template>
+  <!-- ... -->
+  <Modalor />
+</template>
+```
+
+2. create a modal with your own modal component
+
+```ts
+// modal.ts
+import { create } from '@modalor/vue'
+import { h } from 'vue'
+
+interface ModalProps {
+  title: string
+  // ...
+}
+
+// 1. create modal wrapper
+export const createModal = create<ModalProps>(({ onCancel, onOk, open, isOkLoading, props: { renderChildren, ...rest } }) => (
+  h(YourModalComponent, { onCancel, onOk, open, isOkLoading, ...rest }, renderChildren)
+))
+```
+
+3. create a modal with a content component
+
+```ts
+// modals/userProfile.ts
+import { createModal } from './../modal'
+
+// 1.define modal content props
+interface ModalContentProps {
+  username: string
+  // ...
+}
+
+// 2. define modal content resolve values
+interface ModalContentResolveValues {
+  newUsername: string
+  // ...
+}
+// 3. create modal wrapper with modal content props and resolve values
+const userProfileModal = createModal<ModalContentProps, ModalContentResolveValues>(props => (
+  h(YourModalContentComponent, { ...props })
+))
+```
+
+4. use api to show modal
+
+```vue
+<script setup>
+// 1. import your modal
+import { userProfileModal } from '@/modals/userProfile'
+
+async function showModal() {
+  // 2. show modal
+  const [isOk, result] = await userProfileModal.show({
+    username: 'John'
+  }, {
+    title: 'User Profile', // you can override modal props
+  })
+
+  // 3. handle result
+  if (isOk) {
+    console.log(result.newUsername) // will be your resolve values
+  }
+}
+</script>
+
+<template>
+  <button @click="showModal">
+    Show Modal
+  </button>
+</template>
+```
---
+++ packages/react/src/create.ts
@@ -0,0 +1,161 @@
+import { createElement, useEffect, useState } from 'react'
+import { useModalorGlobal } from './Modalor'
+import { useProvideModalor } from './useModalor'
+
+interface ModalOptions<T extends AnyObject> {
+  open: boolean
+  isOkLoading: boolean
+  setOkLoadingStatus: (value: boolean) => void
+  onOk: () => void
+  onCancel: () => void
+  onRemove: () => void
+  props: T & {
+    renderChildren: () => JSX.Element
+  }
+}
+
+interface AnyObject {
+  [key: string]: any
+}
+
+interface CreateOptions<ModalProps extends AnyObject> {
+  transformers?: {
+    modalProps?: (propsWhenCreating: ModalProps, propsWhenShow: Partial<ModalProps>) => ModalProps
+  }
+  /**
+   * 移除组件的延迟时间 (ms)
+   * @default 300
+   */
+  removeDelay?: number
+}
+
+const defaultTransformers = {
+  modalProps: (propsWhenCreating: any, propsWhenShow: any) => ({ ...propsWhenCreating, ...propsWhenShow }),
+}
+
+export function create<ModalProps extends AnyObject>(
+  renderModalWrapper: (options: ModalOptions<ModalProps>) => JSX.Element,
+  options: CreateOptions<ModalProps> = { transformers: defaultTransformers },
+) {
+  return <
+    ModalContentProps extends AnyObject,
+    ResolveValues = unknown,
+    PropsWhenCreating extends AnyObject = ModalProps,
+  >(
+    renderModalContent: (props: ModalContentProps) => JSX.Element,
+    /** createModal 时传入的 modal props */
+    propsWhenCreating: PropsWhenCreating | ((props: ModalContentProps) => PropsWhenCreating) = {} as PropsWhenCreating,
+  ) => {
+    const { create, remove } = useModalorGlobal()
+
+    const Component = ({
+      onOk,
+      onCancel,
+      onClose,
+      ...props
+    }: {
+      onOk?: (values: ResolveValues) => void
+      onCancel?: () => void
+      onClose?: () => void
+      modalProps: Partial<ModalProps>
+      [x: string]: any
+    }) => {
+      const modalorCtx = useProvideModalor()
+      const { modalProps: _modalProps, ...otherProps } = props
+      const propsWhenShow = _modalProps as Partial<ModalProps>
+      const [open, setOpenStatus] = useState(true)
+
+      const handleClose = () => {
+        setOpenStatus(false)
+      }
+
+      const handleOk = async () => {
+        try {
+          await modalorCtx.emit('ok')
+        }
+        catch (error) {
+          console.error(error)
+        }
+      }
+
+      const handleCancel = () => {
+        handleClose()
+        onCancel?.()
+      }
+
+      const handleRemove = () => {
+        onClose?.()
+      }
+
+      useEffect(() => {
+        if (!open) {
+          setTimeout(() => {
+            handleRemove()
+          }, options.removeDelay ?? 300)
+        }
+      }, [open])
+
+      useEffect(() => {
+        if (modalorCtx.isResolved) {
+          onOk?.(modalorCtx.resolvedValues as ResolveValues)
+          handleClose()
+        }
+      }, [modalorCtx.isResolved])
+
+      const renderChildren = () => {
+        const { modalProps, ...otherProps } = props
+        return renderModalContent(otherProps as ModalContentProps)
+      }
+
+      const tmpPropsWhenCreating = (typeof propsWhenCreating === 'function'
+        ? propsWhenCreating(otherProps as ModalContentProps)
+        : propsWhenCreating) as unknown as ModalProps
+
+      const modalProps = options.transformers?.modalProps?.(tmpPropsWhenCreating, propsWhenShow) ?? {
+        ...tmpPropsWhenCreating,
+        ...propsWhenShow,
+      }
+
+      return renderModalWrapper({
+        open,
+        onOk: handleOk,
+        onCancel: handleCancel,
+        onRemove: handleRemove,
+        setOkLoadingStatus: modalorCtx.setOkLoadingStatus,
+        isOkLoading: modalorCtx.isOkLoading,
+        props: {
+          ...modalProps,
+          renderChildren,
+        },
+      })
+    }
+
+    return {
+      show: (
+        props: ModalContentProps = {} as ModalContentProps,
+        /** show modal 时传入的 modal props */
+        modalProps: Partial<ModalProps> = {} as Partial<ModalProps>,
+      ) => {
+        return new Promise<[false, null] | [true, ResolveValues]>((resolve) => {
+          create((id) => {
+            return createElement(Component, {
+              ...props,
+              key: id,
+              id,
+              modalProps,
+              onClose: () => {
+                remove(id)
+              },
+              onCancel: () => {
+                resolve([false, null])
+              },
+              onOk: (values: ResolveValues) => {
+                resolve([true, values])
+              },
+            })
+          })
+        })
+      },
+    }
+  }
+}
